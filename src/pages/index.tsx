0b3730c12f142be2db6b93db7b522a73fadf77b1
Leonardo <leonardo.xavier.clear@gmail.com>
2022-01-24 14:00:10 -0300

    Componente botao Adicionar Novo Cliente

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Cliente from "../../core/Cliente";
+import Botao from "../components/Botao";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 
@@ -27,6 +28,10 @@ export default function Home() {
       text-white
       `}>
       <Layout titulo="Cadastro Simples">
+        <div className="flex justify-end">
+          <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+        </div>
+        
         <Tabela clientes={clientes} 
           clienteSelecionado={clienteSelecionado}
           clienteExcluido={clienteExcluido}
