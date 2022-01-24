57d28ee9520f52e809b60c4bebb3ed4f45f6b3b9
Leonardo <leonardo.xavier.clear@gmail.com>
2022-01-24 17:33:06 -0300

    Configurando repositorio de clientes - Firebase

---
+++ src/firebase/config.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+require('firebase/firestore');
+
+
+if(!firebase.apps.length) {
+
+    firebase.initializeApp({
+        apikey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    })
+}
+
+export default firebase 
+
